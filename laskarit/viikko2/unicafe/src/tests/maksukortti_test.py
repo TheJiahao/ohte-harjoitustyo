412d0b5d0f48682f51e429f3bc6c84c5e22cb740
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-16 10:37:08 +0200

    Add laskari v2t6

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,9 +1,34 @@
 import unittest
 from maksukortti import Maksukortti
 
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_kortin_saldo_alussa_oikein(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_rahan_lataaminen_kasvattaa_saldoa_oikein(self):
+        self.maksukortti.lataa_rahaa(500)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 15.00 euroa")
+
+    def test_kortin_saldo_pienenee_kun_saldoa_on_rahan_ottamiseen(self):
+        self.maksukortti.ota_rahaa(100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 9.00 euroa")
+
+    def test_kortin_saldo_ei_muutu_kun_saldo_ei_riita_rahan_ottamiseen(self):
+        self.maksukortti.ota_rahaa(1100)
+
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_ota_rahaa_palauttaa_true_onnistuessa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100), True)
+
+    def test_ota_rahaa_palauttaa_false_epaonnistuessa(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(1100), False)
