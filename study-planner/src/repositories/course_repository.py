06db505a7815e449f7caf7ae06c6718d91cfab4d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-20 11:28:43 +0200

    Update type hint

--- study-planner/src/repositories/course_repository.py
+++ study-planner/src/repositories/course_repository.py
@@ -5,8 +5,8 @@ class CourseRepository:
     def __init__(self) -> None:
         pass
 
-    def create_course(
-        self, name: str, credits: int, timing: list[bool], requirements: set
+    def create(
+        self, name: str, credits: int, timing: set, requirements: set
     ) -> Course:
         course = Course(name, credits, timing, requirements)
         
