d813f5a9feaf6d70493959416e3f3d53e1409c25
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-21 17:18:25 +0200

    Remove duplicate test file

--- src/tests/entities/course_test.py
+++ src/tests/entities/course_test.py
@@ -1,18 +1,27 @@
-import unittest
 from entities.course import Course
+import unittest
 
 
 class TestCourse(unittest.TestCase):
-    def test_constructor_without_requirements(self):
-        course = Course("Ohjelmistotekniikka", 5, {4})
+    def setUp(self) -> None:
+        pass
 
-        self.assertEqual(course.name, "Ohjelmistotekniikka")
+    def test_course_without_requirements_initialized_correctly(self):
+        course = Course(
+            "Ohte", 5, {3})
+
+        self.assertEqual(course.name, "Ohte")
         self.assertEqual(course.credits, 5)
-        self.assertEqual(course.timing, {4})
+        self.assertEqual(course.timing, {3})
+        self.assertEqual(len(course.requirements), 0)
+        self.assertNotEqual(course.id, None)
 
-    def test_constructor_with_requirements(self):
-        course = Course("Ohjelmistotekniikka", 5, {4})
+    def test_course_without_timing_initialized_correctly(self):
+        course = Course(
+            "Ohte", 5, requirements={"A", "B"})
 
-        self.assertEqual(course.name, "Ohjelmistotekniikka")
+        self.assertEqual(course.name, "Ohte")
         self.assertEqual(course.credits, 5)
-        self.assertEqual(course.timing, {4})
+        self.assertEqual(len(course.timing), 0)
+        self.assertEqual(course.requirements, {"A", "B"})
+        self.assertNotEqual(course.id, None)
