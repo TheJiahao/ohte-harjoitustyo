a0234defd7f3ac3692d604b778480213c9a1cb4d
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-21 15:57:30 +0200

    Add more methods

--- src/repositories/course_repository.py
+++ src/repositories/course_repository.py
@@ -1,13 +1,35 @@
 from entities.course import Course
 from database_connection import get_database_connection
 
+
+def get_course_by_row(row) -> Course | None:
+    if row is None:
+        return None
+
+    return Course(row["name"], row["credits"])
+
 class CourseRepository:
     def __init__(self, connection) -> None:
         self.__connection = connection
+        self.__cursor = self.__connection.cursor()
 
-    def create(
-        self, name: str, credits: int, timing: set, requirements: set
-    ) -> Course:
-        course = Course(name, credits, timing, requirements)
-        
+    def find_all(self) -> list[Course]:
+        courses = self.__cursor.execute("SELECT * FROM Courses").fetchall()
+
+        pass
+
+    def create(self, course: Course) -> Course:
+        self.__cursor.execute("INSERT INTO Courses (id, name, credits) VALUES (?, ?, ?)",
+                              (course.id, course.name, course.credits))
+
+        for period in course.timing:
+            self.__cursor.execute(
+                "INSERT INTO Periods (course_id, period) VALUES (?, ?)", (course.id, period))
+
+        for requirement_id in course.requiments:
+            self.__cursor.execute(
+                "INSERT INTO Requirements (course_id, requirement_id) VALUES (?, ?)", (course.id, requirement_id))
+
+        self.__connection.commit()
+
         return course
