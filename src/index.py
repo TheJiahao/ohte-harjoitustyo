6791f13720641797ac30456dd56e659703020c58
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-29 15:04:38 +0300

    Add index.py

---
+++ src/index.py
@@ -0,0 +1,13 @@
+from tkinter import Tk
+from ui.ui import UI
+
+
+def main():
+    window = Tk()
+    window.title("Study-planner")
+
+    window.mainloop()
+
+
+if __name__ == "__main__":
+    main()
