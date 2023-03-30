b26209560ac6e84017b82d97a342edf34ad846df
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-03-30 20:14:09 +0300

    Refactor Course and add tests
    Initialize tests for CourseRepository

--- src/entities/course.py
+++ src/entities/course.py
@@ -1,61 +1,132 @@
-import uuid
+import copy
 
 
 class Course:
+    """Luokka, joka kuvaa kurssia.
+
+    Attributes:
+        __name (str):
+            Kurssin nimi.
+        __credits (int):
+            Kurssin opintopistemäärä.
+        __timing (list[bool]):
+            Kurssin perioditarjonta tauluna.
+            Oletukseltaan 5-alkioinen taulu False-arvoilla.
+        __requirements (set[int]):
+            Kurssin esitietovaatimuskurssien id:t joukkona.
+            Oletukseltaan tyhjä joukko.
+        __course_id (int):
+            Kurssin id. Oletukseltaan -1.
+    """
+
     def __init__(
         self,
         name: str,
         credits: int,
-        timing: set[int] = set(),
-        requirements: set[str] = set(),
-        course_id: str | None = None,
+        timing: list[bool] | None = None,
+        requirements: set[int] | None = None,
+        course_id: int | None = None,
     ) -> None:
+        """Luokan konstruktori.
+
+        Args:
+            name (str):
+                Kurssin nimi.
+            credits (int):
+                Kurssin opintopistemäärä.
+            timing (list[bool] | None, optional):
+                Kurssin perioditarjonta tauluna. Oletukseltaan None.
+            requirements (set[int] | None, optional):
+                Kurssin esitietovaatimuskurssien id:t joukkona.
+                Oletukseltaan None.
+            course_id (int | None, optional):
+                Kurssin id. Oletukseltaan None.
+        """
         self.__name: str = name
         self.__credits: int = credits
-        self.__timing: set[int] = timing
-        self.__requiments: set[str] = requirements
-        self.__id: str = course_id or str(uuid.uuid4())
+        self.__timing: list[bool] = timing or [False] * 5
+        self.__requiments: set[int] = requirements or set()
+        self.__id: int = course_id or -1
 
     @property
     def name(self) -> str:
         return self.__name
 
-    @name.setter
-    def name(self, name) -> None:
-        self.__name = name
-
     @property
     def credits(self) -> int:
         return self.__credits
 
-    @credits.setter
-    def credits(self, credits) -> None:
-        if credits >= 0:
-            self.__credits = credits
+    @property
+    def timing(self) -> list[bool]:
+        return copy.copy(self.__timing)
 
     @property
-    def timing(self) -> set[int]:
-        return self.__timing
+    def requirements(self) -> set[int]:
+        return copy.copy(self.__requiments)
 
     @property
-    def requirements(self) -> set[str]:
-        return self.__requiments
-
-    @property
-    def id(self) -> str:
+    def id(self) -> int:
         return self.__id
 
-    def add_period(self, period: int) -> None:
-        if period > 0:
-            self.__timing.add(period)
+    @credits.setter
+    def credits(self, credits: int) -> None:
+        """Asettaa opintopistemäärän kurssille.
 
-    def remove_period(self, period: int) -> None:
-        if period in self.__timing:
-            self.__timing.remove(period)
+        Args:
+            credits (int): Asetettava opintopistemäärä.
 
-    def add_requirement(self, id: str) -> None:
+        Raises:
+            ValueError: Opintopistemäärä on ei-positiivinen.
+        """
+        if credits <= 0:
+            raise ValueError("Ei-positiivinen opintopiste ei kelpaa.")
+
+        self.__credits = credits
+
+    @name.setter
+    def name(self, name: str) -> None:
+        """Asettaa uuden nimen kurssille.
+
+        Args:
+            name (str): Asetettava nimi.
+        """
+        self.__name = name
+
+    def set_period(self, period: int, status: bool) -> None:
+        """Asettaa annetun tilan periodille.
+
+        Args:
+            period (int): Muutettava periodi.
+            status (bool): Periodin tila.
+
+        Raises:
+            ValueError: Periodi ei kelpaa.
+        """
+        if not 0 < period < len(self.timing):
+            raise ValueError(f"Periodi {period} ei kelpaa.")
+
+        self.__timing[period] = status
+
+    def add_requirement(self, id: int) -> None:
+        """Lisää esitietokurssin.
+
+        Args:
+            id (int): Lisättävän esitietokurssin id.
+
+        Raises:
+            ValueError: Ei-positiivinen id.
+        """
+        if id <= 0:
+            raise ValueError("Ei-positiivinen id ei kelpaa.")
+
         self.__requiments.add(id)
 
-    def remove_requirement(self, id: str) -> None:
+    def remove_requirement(self, id: int) -> None:
+        """Poistaa esitietokurssin, jos se on esitietovaatimuksissa.
+
+        Args:
+            id (int): Poistettavan esitietokurssin id.
+        """
+
         if id in self.__requiments:
             self.__requiments.remove(id)
--- src/tests/entities/test_course.py
+++ src/tests/entities/test_course.py
@@ -5,22 +5,114 @@ from entities.course import Course
 
 class TestCourse(unittest.TestCase):
     def setUp(self) -> None:
-        pass
+        self.course = Course("Ohte", 5)
 
-    def test_course_without_requirements_initialized_correctly(self):
-        course = Course("Ohte", 5, {3})
-
-        self.assertEqual(course.name, "Ohte")
-        self.assertEqual(course.credits, 5)
-        self.assertEqual(course.timing, {3})
-        self.assertEqual(len(course.requirements), 0)
-        self.assertNotEqual(course.id, None)
-
-    def test_course_without_timing_initialized_correctly(self):
-        course = Course("Ohte", 5, requirements={"A", "B"})
+    def test_course_without_optional_args_initialized_correctly(self):
+        course = Course("Ohte", 5)
 
         self.assertEqual(course.name, "Ohte")
         self.assertEqual(course.credits, 5)
-        self.assertEqual(len(course.timing), 0)
-        self.assertEqual(course.requirements, {"A", "B"})
+        self.assertEqual(course.timing, [False] * 5)
+        self.assertEqual(course.requirements, set())
         self.assertNotEqual(course.id, None)
+
+    def test_course_with_optional_args_initialized_correctly(self):
+        course = Course("Ohte", 5, [True, False, True, False], {2, 3}, 100)
+
+        self.assertEqual(course.name, "Ohte")
+        self.assertEqual(course.credits, 5)
+        self.assertEqual(course.timing, [True, False, True, False])
+        self.assertEqual(course.requirements, {2, 3})
+        self.assertEqual(course.id, 100)
+
+    def test_credit_setter_raises_error_if_nonpositive_value(self):
+        with self.assertRaises(ValueError):
+            self.course.set_period(-100, True)
+            self.course.set_period(-100, False)
+
+        with self.assertRaises(ValueError):
+            self.course.set_period(0, False)
+            self.course.set_period(0, True)
+
+    def test_timing_cannot_be_modified_with_getter(self):
+        self.course.timing[0] = True
+        self.course.timing[4] = True
+
+        self.assertEqual(self.course.timing, [False] * 5)
+
+    def test_requirements_cannot_be_modified_with_getter(self):
+        self.course.requirements.add(100)
+        self.course.requirements.add(0)
+        self.course.requirements.add(-100)
+
+        self.assertEqual(self.course.requirements, set())
+
+    def test_set_period(self):
+        self.course.set_period(1, True)
+        self.course.set_period(2, True)
+        self.course.set_period(4, True)
+
+        self.assertEqual(self.course.timing[1], True)
+        self.assertEqual(self.course.timing[2], True)
+        self.assertEqual(self.course.timing[4], True)
+
+    def test_set_period_raises_error_if_invalid_period(self):
+        with self.assertRaises(ValueError):
+            self.course.set_period(100, True)
+
+        with self.assertRaises(ValueError):
+            self.course.set_period(0, False)
+
+        with self.assertRaises(ValueError):
+            self.course.set_period(-10, True)
+
+    def test_add_requirements(self):
+        self.course.add_requirement(2)
+        self.course.add_requirement(4)
+        self.course.add_requirement(10)
+
+        self.assertEqual(self.course.requirements, {2, 4, 10})
+
+    def test_add_requirements_raises_error_if_invalid_id(self):
+        with self.assertRaises(ValueError):
+            self.course.add_requirement(-100)
+
+        with self.assertRaises(ValueError):
+            self.course.add_requirement(0)
+
+    def test_remove_requirement(self):
+        self.course.add_requirement(2)
+        self.course.add_requirement(5)
+        self.course.add_requirement(10)
+
+        self.course.remove_requirement(5)
+
+        self.assertEqual(self.course.requirements, {2, 10})
+
+    def test_remove_requirements_does_nothing_if_id_not_in_requirements(self):
+        self.course.add_requirement(1)
+        self.course.add_requirement(2)
+        self.course.add_requirement(10)
+
+        self.course.remove_requirement(-1000)
+        self.course.remove_requirement(0)
+        self.course.remove_requirement(123)
+
+        self.assertEqual(self.course.requirements, {1, 2, 10})
+
+    def test_credits_setter(self):
+        self.course.credits = 100
+
+        self.assertEqual(self.course.credits, 100)
+
+    def test_credits_setter_raises_error_if_nonpositive_credit(self):
+        with self.assertRaises(ValueError):
+            self.course.credits = -100
+
+        with self.assertRaises(ValueError):
+            self.course.credits = 0
+
+    def test_name_setter(self):
+        self.course.name = "OhJa"
+
+        self.assertEqual(self.course.name, "OhJa")
