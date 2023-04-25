a44e440b8258d9f239d7551331f015c221ded56c
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-04-25 14:25:39 +0300

    Add build task

---
+++ src/build.py
@@ -0,0 +1,5 @@
+from initialize_database import initialize_database
+
+
+def build() -> None:
+    initialize_database()
--- tasks.py
+++ tasks.py
@@ -26,3 +26,8 @@ def format(ctx):
 @task
 def lint(ctx):
     ctx.run("pylint src", pty=True)
+
+
+@task
+def build(ctx):
+    ctx.run("python3 src/build.py", pty=True)
