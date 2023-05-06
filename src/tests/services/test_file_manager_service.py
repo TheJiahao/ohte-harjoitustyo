8467f4549cc3de6a01e1dfe93492b737fa4434ce
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-05-06 19:34:52 +0300

    Refactor FileManagerService

--- src/tests/services/test_file_manager_service.py
+++ src/tests/services/test_file_manager_service.py
@@ -20,3 +20,15 @@ class TestFileManagerService(unittest.TestCase):
         self.assertEqual(courses[0], Course("A", 5, {2}, course_id=1))
         self.assertEqual(courses[1], Course("B", 5, {1, 4}, {1}, course_id=2))
         self.assertEqual(courses[2], Course("C", 10, {3}, {1}, course_id=3))
+
+    def test_write(self):
+        a = Course("A", 10, {2}, course_id=1)
+        b = Course("B", 10, {2}, {1}, course_id=2)
+
+        file = os.path.join(self.data_directory, "test_output.json")
+
+        file_manager_service.write([a, b], file)
+
+        courses = file_manager_service.read(file)
+
+        self.assertEqual(list(courses), [a, b])
