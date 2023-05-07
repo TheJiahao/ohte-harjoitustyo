9419aff563962b4c8e112f580342c56ec4982fe1
Jiahao Li <42709836+TheJiahao@users.noreply.github.com>
2023-05-07 11:28:38 +0300

    Add more test for ExportService

--- src/tests/services/test_export_service.py
+++ src/tests/services/test_export_service.py
@@ -11,15 +11,21 @@ class TestExportService(unittest.TestCase):
         dirname = os.path.dirname(__file__)
 
         self.data_directory = os.path.join(dirname, "..", "data")
+        self.file = os.path.join(self.data_directory, "test_output.json")
 
     def test_write(self):
         a = Course("A", 10, {2}, course_id=1)
         b = Course("B", 10, {2}, {1}, course_id=2)
 
-        file = os.path.join(self.data_directory, "test_output.json")
-
-        export_service.write([a, b], file)
-
-        courses = import_service.read(file)
+        export_service.write([a, b], self.file)
+        courses = import_service.read(self.file)
 
         self.assertEqual(list(courses), [a, b])
+
+    def test_write_empty_list(self):
+        export_service.write([], self.file)
+
+        with open(self.file, mode="r", encoding="utf-8") as file:
+            data = file.read()
+
+        self.assertEqual(data, "[]")
